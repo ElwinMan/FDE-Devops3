95c2541f94aa04b28ad9c93e6dee757bc25506dc
ELwin Man <man0008@hz.nl>
2022-06-19 18:28:32 +0200

    changed nav and faq/index

--- resources/views/faq/index.blade.php
+++ resources/views/faq/index.blade.php
@@ -1,18 +1,28 @@
-@extends('layout')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('FAQ') }}
+        </h2>
+    </x-slot>
 
-@section('content')
-    <main>
-        <h1>FAQ</h1>
-        <div>
-            <h2><a href="{{ route('faq.create') }}">Create New FAQ</a></h2>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <h1>FAQ</h1>
+                    <div>
+                        <h2><a href="{{ route('faq.create') }}">Create New FAQ</a></h2>
+                    </div>
+                    <ul>
+                        @foreach($faqs as $faq)
+                            <li>{{ $faq->question }}</li>
+                            {{ $faq->answer }}
+                            <h4><a href="{{ route('faq.edit', $faq) }}">Edit</a></h4>
+                            <hr>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
         </div>
-        <ul>
-            @foreach($faqs as $faq)
-                <li>{{ $faq->question }}</li>
-                {{ $faq->answer }}
-                <h4><a href="{{ route('faq.edit', $faq) }}">Edit</a></h4>
-                <hr>
-            @endforeach
-        </ul>
-    </main>
-@endsection
+    </div>
+</x-app-layout>
