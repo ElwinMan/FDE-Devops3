19d654f234e5438ab5735bec08a7a7787f24a4af
ELwin Man <man0008@hz.nl>
2022-06-19 19:09:43 +0200

    change FAQ nav linking

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,7 +15,7 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <a class="{{ Request::path() === 'faq' ? 'current_page_item' : ''}}" href='faq'>FAQs</a>
+                    <a href=/faq class="{{ Request::is('faq*') ? 'current_page_item' : '' }}">FAQ</a>
                 </div>
             </div>
 
